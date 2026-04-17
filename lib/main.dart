ad90af8cf02647a14263dede5cb1dfbb3f11d70f
muhammadalfinrohmatulloh07-create <muhammadalfinrohmatulloh123@gmail.com>
2026-04-17 10:06:33 +0700

    alfin_menambahkan detail menu produk

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ import 'Front end/keranjang.dart';
 import 'Front end/konfirmasipesanan.dart';
 import 'Front end/lupa_password.dart';
 import 'Front end/admin.dart' ;
-import 'Front end/product_detail.dart' ;
+import 'Front end/bukti_pesanan.dart';
+import 'Front end/product_detail.dart';
+
 
 void main() {
   runApp(const PuddingkuApp()); // Tambah const biar performanya lebih ringan
